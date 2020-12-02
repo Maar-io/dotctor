c1987e3e276141aef381e0b53a2cdf8401350548
Mario <mariovukelic@yahoo.com>
2020-12-02 21:50:57 +0100

    removed Gecko

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import CoinList from './components/CoinList';
 import GetGithub from './components/GetGithub';
 import Home from './components/Home';
 import { AuthProvider } from "./components/AuthContext"
@@ -16,40 +15,11 @@ import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute'
 
 import styled from 'styled-components';
-import axios from 'axios';
 
 function App(props) {
   console.log("render APP")
-  const [coinData, setCoinData] = useState([]);
   const [isSignedIn, setSignedIn] = useState(false);
   const [apolloClient, setApolloClient] = useState({});
-
-  const componentDidMount = async () => {
-    const dotIds = ['kusama', 'polkadot', 'edgeware', 'sora', 'chainx', 'darwinia-network-native-token', 'akropolis', 'mantra-dao', 'robonomics-network', 'polkastarter', 'stafi', 'kulupu', 'robonomics-web-services', 'chads-vc'];
-    const dotUrl = 'https://api.coingecko.com/api/v3/coins/';
-    const promises = dotIds.map(id => axios.get(dotUrl + id));
-    const coinData = await Promise.all(promises);
-    const coinPriceData = coinData.map(function (response) {
-      const coin = response.data;
-      return {
-        key: coin.id,
-        name: coin.name,
-        ticker: coin.symbol,
-        homepage: coin.links.homepage[0],
-        icon: coin.image.thumb,
-        rank: coin.market_cap_rank,
-        marketcap: coin.market_data.market_cap.usd,
-        price: coin.market_data.current_price.usd,
-        github: coin.links.repos_url.github
-      };
-
-    })
-      .sort(function (a, b) {
-        return a.rank - b.rank;
-      });
-    setCoinData(coinPriceData);
-
-  }
 
   function tokenReceived(token) {
     setSignedIn(true);
@@ -76,13 +46,6 @@ function App(props) {
     setApolloClient(client)
   }
 
-  useEffect(function () {
-    if (coinData.length === 0) {
-      componentDidMount();
-    }
-
-  }, []);
-
   return (
 
     <React.Fragment>
@@ -93,7 +56,6 @@ function App(props) {
         <AuthProvider>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/gecko" render={(props) => <CoinList {...props} coinData={coinData} />} />
             <Route path="/login" component={Login} />
             <PrivateRoute path="/github" component={GetGithub} />
           </Switch>
